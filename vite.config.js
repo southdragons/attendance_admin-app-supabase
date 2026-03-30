417fb0a8b1496524367cbb226f81e144d3a35765
kakimon <taku.umi721@gmail.com>
2026-03-30 21:46:22 +0900

    feat: 通知修正

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/attendance_admin-app/', // 🔥これ追加
   plugins: [vue()],
   server: {
     proxy: {
