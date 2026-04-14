75f062835b6c8ef9a2232e64651f05fd4e0944d1
southdragons <kaki.workmon@gmail.com>
2026-04-14 14:17:23 +0900

    feat: update vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/attendance_admin-app/', // 🔥これ追加
+  base: '/attendance_admin-app-supabase/',
   plugins: [vue()],
-  server: {
-    proxy: {
-      '/gas': {
-        target: 'https://script.google.com',
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/gas/, ''),
-      },
-    },
-  },
 })
